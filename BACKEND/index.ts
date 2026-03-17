9b7de6ef323e3f292ee3407d97ac614487d040be
Arón <aronpablo12@gmail.com>
2026-03-17 13:47:31 -0300

    fix: port configuration for deploy

--- BACKEND/index.ts
+++ BACKEND/index.ts
@@ -1,18 +1,18 @@
-
 import express, { Application } from "express";
 import cors from "cors";
-import router from "./router/users"
+import router from "./router/users";
 
 const app: Application = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(router);
-const PORT: number = 8080;
 
-app.listen(PORT, () =>
+// Render asigna un puerto automáticamente en la variable de entorno PORT
+// Si no existe (como en tu PC), usará el 8080
+const PORT = process.env.PORT || 8080;
+
+// Es importante agregar '0.0.0.0' para que Render pueda dirigir el tráfico al contenedor
+app.listen(Number(PORT), "0.0.0.0", () =>
   console.log(`🚀 SERVIDOR ESCUCHANDO EN EL PUERTO ${PORT}`)
 );
-
-
-
