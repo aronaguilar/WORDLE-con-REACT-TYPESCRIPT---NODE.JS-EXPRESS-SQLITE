33b8ee646299962f01a7de1446b6d14ddddac7f5
Arón <aronpablo12@gmail.com>
2026-03-17 14:52:26 -0300

    listo

--- FRONTEND/mi-frontend/src/PAGINAS/Jugando.tsx
+++ FRONTEND/mi-frontend/src/PAGINAS/Jugando.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import "../PAGINAS STYLE/Jugando.css";
+import "../PAGINAS STYLE/jugando.css";
 import FondoVideo from '../COMPONENTES/FondoVideo';
 import Header from '../COMPONENTES/Header';
 import ContLetra from '../COMPONENTES/ContLetra';
